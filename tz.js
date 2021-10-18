0a3d1b6be3ec2e8b4986d75f3a45cf8dab2cf3d3
shaheerashan <77646573+shaheerashan@users.noreply.github.com>
2021-10-17 22:19:04 -0400

    Edit console and function

--- tz.js
+++ tz.js
@@ -1,7 +1,24 @@
 const moment = require('moment-timezone');
+// eslint-disable-next-line import/no-unresolved
+const yargs = require('yargs');
 
 moment.tz.setDefault('America/New_York');
 
-const targetTimezone = 'Europe/Paris';
+let targetTimezone;
+let params;
 
-console.log(moment().tz(targetTimezone).format());
+// eslint-disable-next-line eqeqeq
+if (process.argv.length != 3) {
+  console.log('Usage: node <script-file> <timezone>');
+  process.exit(1);
+} else {
+  params = yargs.argv;
+  // eslint-disable-next-line prefer-destructuring
+  targetTimezone = params._[0];
+}
+
+console.log(
+  `The time at the ${targetTimezone} timezone is ${moment()
+    .tz(targetTimezone)
+    .format()}`
+);
